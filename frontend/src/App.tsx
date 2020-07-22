a01eba50cd4ceb53a03382e5a42ca272fda9d54f
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-07-22 21:30:22 +0300

    added backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, FC, useEffect } from "react";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar";
+import api from "./api/api";
+import { ApiResponse } from "apisauce";
 
-function App() {
-  return <NavigationBar title="Это навигацияяяя" />;
+interface HelloWorld {
+  id: number;
+  content: string;
 }
+
+const App = () => {
+  const [helloWorld, setHelloWorld] = useState<HelloWorld | unknown>({
+    id: 0,
+    content: "",
+  });
+
+  useEffect(() => {
+    api
+      .get("/greeting")
+      .then((res) => res.data)
+      .then((res: HelloWorld | unknown) => setHelloWorld(res));
+  }, []);
+
+  return (
+    <React.Fragment>
+      <NavigationBar title="Это навигацияяяя" />
+      <h1>
+        id: {(helloWorld as HelloWorld).id} content:{" "}
+        {(helloWorld as HelloWorld).content}
+      </h1>
+    </React.Fragment>
+  );
+};
 
 export default App;
