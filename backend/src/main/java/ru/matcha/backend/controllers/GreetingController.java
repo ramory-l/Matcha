f7150c567c2fbea94080fd7e1f0613580aa3857c
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-07-23 14:40:05 +0300

    better springdockerfile, but not best

--- backend/src/main/java/ru/matcha/backend/controllers/GreetingController.java
+++ backend/src/main/java/ru/matcha/backend/controllers/GreetingController.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @CrossOrigin(origins = "http://localhost:3000")
