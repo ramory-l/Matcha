06b7f87dfaee2e5b45929e1d044b9b4f9ed46d98
artemtulupov <tulupov1995@gmail.com>
2020-10-11 15:26:01 +0300

    adding script for starting server v2 (with param)

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-mvn clean dependency:copy-dependencies package
-java -cp target/matcha-0.0.1-SNAPSHOT.jar ru.school.matcha.MatchaApplication
+
+if [ -n "$1" ]; then
+  case "$1" in
+  -c) mvn clean ;;
+  -s) java -cp target/matcha-0.0.1-SNAPSHOT.jar ru.school.matcha.MatchaApplication ;;
+  -b) mvn clean dependency:copy-dependencies package ;;
+  -all)
+    mvn clean dependency:copy-dependencies package
+    java -cp target/matcha-0.0.1-SNAPSHOT.jar ru.school.matcha.MatchaApplication
+    ;;
+  esac
+else
+  echo "-b - build; -s - start; -all - build and start; -c - clean"
+fi
