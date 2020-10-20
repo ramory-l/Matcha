3cb13c1343d6c039c78cc319b178fc7ae08818e7
artemtulupov <tulupov1995@gmail.com>
2020-10-21 00:58:05 +0300

    new docker-compose and deploy

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 docker run -it --rm --name backend -v "$(pwd)/backend":/usr/src/backend -w /usr/src/backend maven:3.3-jdk-8 mvn clean compile assembly:single
 mv backend/target/Matcha.jar backend/docker
 rm -rf backend/target
-docker run -it --rm --name generator -v "$(pwd)/generator":/usr/src/generator -w /usr/src/generator maven:3.3-jdk-8 mvn clean compile assembly:single
-mv generator/target/usersGenerator.jar backend/docker
-rm -rf generator/target
+docker run -it --rm --name generator -v "$(pwd)/backend/generator":/usr/src/generator -w /usr/src/generator maven:3.3-jdk-8 mvn clean compile assembly:single
+mv backend/generator/target/usersGenerator.jar backend/docker
+rm -rf backend/generator/target
 cp backend/src/main/resources/db/sql/init_db.sql backend/docker
 docker-compose build
 mkdir backend/jars
