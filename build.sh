a02e87366c32b5501d1976a8ccda5526c47493ea
artemtulupov <tulupov1995@gmail.com>
2020-10-20 19:41:08 +0300

    create build.sh for dockerMaven

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf ./jars
+mkdir jars
+docker run -it --rm --name backend -v "$(pwd)/backend":/usr/src/backend -w /usr/src/backend maven:3.3-jdk-8 mvn clean compile assembly:single
+mv ./backend/target/Matcha.jar ./jars/
+rm -rf backend/target
+docker run -it --rm --name generator -v "$(pwd)/generator":/usr/src/generator -w /usr/src/generator maven:3.3-jdk-8 mvn clean compile assembly:single
+mv ./generator/target/usersGenerator.jar ./jars/
+rm -rf generator/target
