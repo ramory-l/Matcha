96a51bba5faaff88d6be812fe55cddd086903c48
artemtulupov <tulupov1995@gmail.com>
2020-10-20 20:38:29 +0300

    Dockerfiles are OK and mini edit init_db.sql + pom.xml

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ rm -rf backend/target
 docker run -it --rm --name generator -v "$(pwd)/generator":/usr/src/generator -w /usr/src/generator maven:3.3-jdk-8 mvn clean compile assembly:single
 mv ./generator/target/usersGenerator.jar ./jars/
 rm -rf generator/target
+docker build -t backend . --file backend/Dockerfile
+docker build -t database . --file db/Dockerfile
