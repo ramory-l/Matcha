4ee3c5c97da1e5a27e9406f844a8e9220e185f89
Artem Tulupov <tulupov1995@gmail.com>
2020-10-23 00:16:46 +0300

    option endpoint + forming headers

--- backend/src/main/java/ru/school/matcha/MatchaApplication.java
+++ backend/src/main/java/ru/school/matcha/MatchaApplication.java
@@ -397,12 +397,18 @@ public class MatchaApplication {
                 }, new JsonTransformer());
             });
         });
-        after((req, res) -> {
-            res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "*");
-            res.header("Access-Control-Allow-Credentials", "true");
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", "content-type,x-auth-token");
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            }
+            return request.body();
         });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
     }
 
 }
