f939233ffd01106400668328bf05a6605206f2da
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-11-03 22:21:10 +0300

    socket test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,28 @@ import { ToastContainer } from "react-toastify";
 import AuthPage from "./pages/authPage";
 import BasePage from "./pages/basePage";
 import ProtectedRoute from "./components/common/protectedRoute";
+import auth from "./services/authService";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
+  let socket = new WebSocket(
+    `ws://localhost:8080/socket?x-auth-token=T_${auth.getJwt()}`
+  );
+
+  console.log(socket);
+
+  socket.onopen = function (e) {
+    console.log("Socket ready");
+    socket.send("HEEELLO");
+  };
+
+  socket.onmessage = function (e) {
+    let message = e.data;
+
+    console.log(message);
+  };
+
   return (
     <>
       <ToastContainer />
