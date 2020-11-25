e263fba8e536ceecd7b4766b4559834237ff6ddb
Artem Tulupov <tulupov1995@gmail.com>
2020-11-25 23:51:28 +0300

    fix exceptions

--- backend/src/main/java/ru/school/matcha/enums/Response.java
+++ backend/src/main/java/ru/school/matcha/enums/Response.java
@@ -11,11 +11,11 @@ public enum Response {
     GET(org.eclipse.jetty.server.Response.SC_OK, null),
     PUT(org.eclipse.jetty.server.Response.SC_NO_CONTENT, null),
     DELETE(org.eclipse.jetty.server.Response.SC_NO_CONTENT, null),
-    AUTHENTICATION_EXCEPTION(org.eclipse.jetty.server.Response.SC_FORBIDDEN, "Authentication failed"),
-    NUMBER_FORMAT_EXCEPTION(org.eclipse.jetty.server.Response.SC_BAD_REQUEST, "Invalid number passed"),
-    NOT_FOUND_EXCEPTION(org.eclipse.jetty.server.Response.SC_NOT_FOUND, "Not found"),
+    AUTHENTICATION_EXCEPTION(org.eclipse.jetty.server.Response.SC_UNAUTHORIZED, "Authentication is failed"),
+    NUMBER_FORMAT_EXCEPTION(org.eclipse.jetty.server.Response.SC_BAD_REQUEST, "Invalid number is passed"),
+    NOT_FOUND_EXCEPTION(org.eclipse.jetty.server.Response.SC_NOT_FOUND, "Resource not found"),
     MATCHA_EXCEPTION(org.eclipse.jetty.server.Response.SC_BAD_REQUEST, "Bad request"),
-    EXCEPTION(org.eclipse.jetty.server.Response.SC_INTERNAL_SERVER_ERROR, "An unexpected error");
+    EXCEPTION(org.eclipse.jetty.server.Response.SC_INTERNAL_SERVER_ERROR, "An unexpected error on server");
 
     private final int status;
     private final String body;
