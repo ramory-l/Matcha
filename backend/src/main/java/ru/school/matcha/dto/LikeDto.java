9a5f561070cdeaa4287192a4026adeb4637c6949
Artem Tulupov <tulupov1995@gmail.com>
2020-12-08 01:09:05 +0300

    fix guests

--- backend/src/main/java/ru/school/matcha/dto/LikeDto.java
+++ backend/src/main/java/ru/school/matcha/dto/LikeDto.java
@@ -2,13 +2,12 @@ package ru.school.matcha.dto;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Data;
-import ru.school.matcha.domain.Image;
 
 @Data
 @JsonAutoDetect
 public class LikeDto {
 
-    private Image avatar;
+    private ImageDto avatar;
     private String username;
     private Long id;
 
